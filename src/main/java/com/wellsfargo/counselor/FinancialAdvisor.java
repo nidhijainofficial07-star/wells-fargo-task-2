94ceeef3c885016f5858633f655f2b8430f9c892
nidhijainofficial07-star <nidhijain.official07@gamil.com>
2026-06-18 13:28:54 +0530

    task 2

---
+++ src/main/java/com/wellsfargo/counselor/FinancialAdvisor.java
@@ -0,0 +1,71 @@
+package com.wellsfargo.counselor;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class FinancialAdvisor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long advisorId;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String email;
+
+    @OneToMany(mappedBy = "advisor")
+    private List<Client> clients;
+
+    public FinancialAdvisor(Long advisorId, String firstName, String lastName,
+                            String email, List<Client> clients) {
+        this.advisorId = advisorId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.clients = clients;
+    }
+
+    public FinancialAdvisor() {}
+
+    public Long getAdvisorId() {
+        return advisorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
+    }
+}
