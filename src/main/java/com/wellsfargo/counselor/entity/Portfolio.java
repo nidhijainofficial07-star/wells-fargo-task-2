4eeedcb79b5084afa7e587fa8d4adbfe2fbac072
nidhijainofficial07-star <nidhijain.official07@gamil.com>
2026-06-18 13:50:54 +0530

    task 2

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,48 @@
+package com.jpmc.midascore.entity;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long portfolioId;
+
+    @OneToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @OneToMany(mappedBy = "portfolio")
+    private List<Security> securities;
+
+    public Portfolio(Long portfolioId, Client client,
+                     List<Security> securities) {
+        this.portfolioId = portfolioId;
+        this.client = client;
+        this.securities = securities;
+    }
+
+    public Portfolio() {}
+
+    public Long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<Security> getSecurities() {
+        return securities;
+    }
+
+    public void setSecurities(List<Security> securities) {
+        this.securities = securities;
+    }
+}
